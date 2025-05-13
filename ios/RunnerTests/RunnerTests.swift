d35348c8577af0d857e6b33e880fbbb3057add26
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2025-05-13 23:14:10 +0530

    Projected updated to run on ios

---
+++ ios/RunnerTests/RunnerTests.swift
@@ -0,0 +1,16 @@
+//
+//  RunnerTests.swift
+//  RunnerTests
+//
+//  Created by Nirmal Yohannan on 13/05/25.
+//
+
+import Testing
+
+struct RunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
